c6e25ae6f94c289ea684e4135ff497fcaaa0123f
Nubstex <91033840+Nubstex@users.noreply.github.com>
2021-09-22 22:09:24 +0200

    Update start.py

--- start.py
+++ start.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python
 import os
 import subprocess
+
 print("start")
+
 os.system("chmod +rwx -v ./start.sh")
 subprocess.call(['sh', './start.sh'])
+
 os.system("chmod +rwx -v ./Nubot-code/run.py")
 os.chdir('Nubot-code')
-os.system('python run.py')
+
+key = 'TOKEN'
+value = os.getenv('TOKEN')
+print("Value of 'TOKEN' environment variable :", value) 
+
+my_env = os.environ.copy()
+my_env["PATH"] = "/usr/bin/env:" + my_env["PATH"]
+subprocess.Popen('python run.py', env=my_env)
+
 print("end")
