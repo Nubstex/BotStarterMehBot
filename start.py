8c677dd2ae2cfe66377806bc4b805c9e35f2e198
Nubstex <91033840+Nubstex@users.noreply.github.com>
2021-09-22 20:36:37 +0200

    Update start.py

--- start.py
+++ start.py
@@ -5,5 +5,5 @@ print("start")
 os.system("chmod +rwx -v ./start.sh")
 subprocess.call(['sh', './start.sh'])
 os.system("chmod +rwx -v ./Nubot-code/run.py")
-python "./Nubot-code/run.py"
+python ./Nubot-code/run.py
 print("end")
